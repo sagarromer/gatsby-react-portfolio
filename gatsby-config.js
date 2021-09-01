8cf68684935693b5df15976029de26d79a67a55c
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-08-31 23:16:57 -0700

    jobs section created

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,9 +24,10 @@ module.exports = {
         //   contentTypes : `jobs`, `projects`, `blogs`,
         //   singleType : `about`
         //  ONLY ADD TO ARRAY IF YOU HAVE DATA IN STRAPI !!!!
-        contentTypes: [],
+        contentTypes: [`jobs`],
         singleTypes: [],
-      }},
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,13 @@ import Layout from "../components/Layout2"
 import Hero from "../components/Hero"
 import Seo from "../components/seo"
 import Services from "../components/Services"
+import Jobs from "../components/Jobs"
 
 const IndexPage = () => (
   <Layout>
     <Hero />
     <Services />
+    <Jobs />
   </Layout>
 )
 
