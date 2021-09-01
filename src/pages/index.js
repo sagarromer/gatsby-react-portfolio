bdf063e680124f51eeafc59f49e8b1854bdc5fc6
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-09-01 01:07:33 -0700

    featured and all projects section created

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,54 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import { graphql } from "gatsby"
 import Layout from "../components/Layout2"
 import Hero from "../components/Hero"
 import Seo from "../components/seo"
 import Services from "../components/Services"
 import Jobs from "../components/Jobs"
+import Projects from "../components/Projects"
 
-const IndexPage = () => (
-  <Layout>
-    <Hero />
-    <Services />
-    <Jobs />
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const {
+    allStrapiProjects: { nodes: projects },
+  } = data
+  return (
+    <>
+      <Seo title="Home" />
+      <main>
+        <Hero />
+        <Services />
+        <Jobs />
+        <Projects title="featured projects" showLink projects={projects} />
+      </main>
+    </>
+  )
+}
+
+export const query = graphql`
+  {
+    allStrapiProjects(filter: { featured: { eq: true } }) {
+      nodes {
+        description
+        featured
+        github
+        id
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
 
 export default IndexPage
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,47 @@
 import React from "react"
-import Layout from "../components/Layout"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
-// ...GatsbyImageSharpFluid
-
-const ProjectsPage = () => {
-    return <h2>projects page</h2>
+import Seo from "../components/Seo"
+const ProjectsPage = ({
+  data: {
+    allStrapiProjects: { nodes: projects },
+  },
+}) => {
+  return (
+    <>
+      <Seo title="Projects" />
+      <main>
+        <section className="projects-page">
+          <Projects title="all projects" projects={projects} />
+        </section>
+      </main>
+    </>
+  )
 }
+
+export const query = graphql`
+  {
+    allStrapiProjects {
+      nodes {
+        description
+        featured
+        github
+        id
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
 
 export default ProjectsPage
