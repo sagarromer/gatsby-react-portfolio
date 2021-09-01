3cd883cb6432e7cb0b75d5559647f5b35628755e
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-09-01 08:45:54 -0700

    react markdown prop changed

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -9,11 +9,10 @@ const ComponentName = ({ data }) => {
 
   return (
     <Layout>
-      <SEO title={title} description={desc} />
       <section className="blog-template">
         <div className="section-center">
           <article className="blog-content">
-            <ReactMarkdown source={content} />
+            <ReactMarkdown children={content} />
           </article>
           <Link to="/blog" className="btn center-btn">
             blog
