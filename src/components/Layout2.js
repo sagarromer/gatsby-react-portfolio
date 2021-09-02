a908045b8946eb7b2f268928c673ab8a109a48e8
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-09-02 03:01:30 -0700

    sidebar animation completed

--- src/components/Layout2.js
+++ src/components/Layout2.js
@@ -4,9 +4,14 @@ import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const toggleSlidebar = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSlidebar} />
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSlidebar} />
       {children}
       <Footer />
     </>
